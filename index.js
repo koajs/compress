6f496636db80bdebc7a2ce73c90d09379d84254e
Jonathan Ong <jonathanrichardong@gmail.com>
2014-04-25 00:33:23 -0700

    refactor again and use statuses

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 var compressible = require('compressible')
 var isJSON = require('koa-is-json')
+var status = require('statuses')
 var Stream = require('stream')
 var bytes = require('bytes')
 var zlib = require('zlib')
@@ -41,16 +42,11 @@ module.exports = function (options) {
     yield* next
 
     var body = this.body
-
-    if (this.compress === false
-      || this.method === 'HEAD'
-      || this.status === 204
-      || this.status === 304
-      // Assumes you either always set a body or always don't
-      || body == null
-      // Skip if content-encoding is already set
-      || this.response.get('Content-Encoding')
-    ) return
+    if (!body) return
+    if (this.compress === false) return
+    if (this.request.method === 'HEAD') return
+    if (status.empty[this.response.status]) return
+    if (this.response.get('Content-Encoding')) return
 
     // forced compression or implied
     if (!(this.compress === true || filter(this.response.type))) return
@@ -60,26 +56,22 @@ module.exports = function (options) {
     if (!encoding) this.throw(406, 'supported encodings: gzip, deflate, identity')
     if (encoding === 'identity') return
 
+    // json
+    if (isJSON(body)) body = this.body = JSON.stringify(body)
+
     // threshold
     if (threshold && this.response.length < threshold) return
-
-    // json
-    if (isJSON(body)) {
-      body = JSON.stringify(body)
-      if (threshold && body.length < threshold) return
-    }
 
     this.set('Content-Encoding', encoding)
     this.res.removeHeader('Content-Length')
 
-    var stream = encodingMethods[encoding](options)
+    var stream =
+    this.body = encodingMethods[encoding](options)
 
     if (body instanceof Stream) {
-      body.on('error', this.onerror).pipe(stream)
+      body.pipe(stream)
     } else {
       stream.end(body)
     }
-
-    this.body = stream
   }
 }
