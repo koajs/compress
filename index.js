9db7bbbd502da9372101bba26e26a3f11bbdac36
tinovyatkin <tino@vtkn.io>
2017-03-03 00:02:34 -0400

    refactor to ES6 / async / await

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-'use strict';
-
 /**
  * Module dependencies.
  */
 
-var compressible = require('compressible')
-var isJSON = require('koa-is-json')
-var status = require('statuses')
-var Stream = require('stream')
-var bytes = require('bytes')
-var zlib = require('zlib')
+const compressible = require('compressible')
+const isJSON = require('koa-is-json')
+const status = require('statuses')
+const Stream = require('stream')
+const bytes = require('bytes')
+const zlib = require('zlib')
 
 /**
  * Encoding methods supported.
  */
 
-var encodingMethods = {
+const encodingMethods = {
   gzip: zlib.createGzip,
   deflate: zlib.createDeflate
 }
@@ -28,52 +26,45 @@ var encodingMethods = {
  * @api public
  */
 
-module.exports = (options) => {
-  options = options || {}
+module.exports = (options = {}) => {
+  let { filter = compressible, threshold = 1024 } = options;
+  if (typeof threshold === 'string') threshold = bytes(threshold);
 
-  var filter = options.filter || compressible
-
-  var threshold = !options.threshold ? 1024
-    : typeof options.threshold === 'number' ? options.threshold
-    : typeof options.threshold === 'string' ? bytes(options.threshold)
-    : 1024
-
-  return function compress(ctx, next) {
+  return async (ctx, next) => {
     ctx.vary('Accept-Encoding')
 
-    return next().then(() => {
-      var body = ctx.body
-      if (!body) return
-      if (ctx.compress === false) return
-      if (ctx.request.method === 'HEAD') return
-      if (status.empty[ctx.response.status]) return
-      if (ctx.response.get('Content-Encoding')) return
+    await next()
+    
+    let { body } = ctx;
+    if (!body) return
+    if (ctx.compress === false) return
+    if (ctx.request.method === 'HEAD') return
+    if (status.empty[ctx.response.status]) return
+    if (ctx.response.get('Content-Encoding')) return
 
-      // forced compression or implied
-      if (!(ctx.compress === true || filter(ctx.response.type))) return
+    // forced compression or implied
+    if (!(ctx.compress === true || filter(ctx.response.type))) return
 
-      // identity
-      var encoding = ctx.acceptsEncodings('gzip', 'deflate', 'identity')
-      if (!encoding) ctx.throw(406, 'supported encodings: gzip, deflate, identity')
-      if (encoding === 'identity') return
+    // identity
+    const encoding = ctx.acceptsEncodings('gzip', 'deflate', 'identity')
+    if (!encoding) ctx.throw(406, 'supported encodings: gzip, deflate, identity')
+    if (encoding === 'identity') return
 
-      // json
-      if (isJSON(body)) body = ctx.body = JSON.stringify(body)
+    // json
+    if (isJSON(body)) body = ctx.body = JSON.stringify(body)
 
-      // threshold
-      if (threshold && ctx.response.length < threshold) return
+    // threshold
+    if (threshold && ctx.response.length < threshold) return
 
-      ctx.set('Content-Encoding', encoding)
-      ctx.res.removeHeader('Content-Length')
+    ctx.set('Content-Encoding', encoding)
+    ctx.res.removeHeader('Content-Length')
 
-      var stream =
-      ctx.body = encodingMethods[encoding](options)
+    const stream = ctx.body = encodingMethods[encoding](options)
 
-      if (body instanceof Stream) {
-        body.pipe(stream)
-      } else {
-        stream.end(body)
-      }
-    });
+    if (body instanceof Stream) {
+      body.pipe(stream)
+    } else {
+      stream.end(body)
+    }
   };
 }
