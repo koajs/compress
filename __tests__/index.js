f7cf2f7ab1997afb65204ff80757a89fe78f7c79
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2019-11-03 23:23:35 -0600

    Fixing ESLint gripes in a test function.

--- __tests__/index.js
+++ __tests__/index.js
@@ -363,7 +363,7 @@ describe('Compress', () => {
   it('accept-encoding: br (banned, should be gzip)', (done) => {
     const app = new Koa()
 
-    app.use(compress({brotliOptions: null}))
+    app.use(compress({ brotliOptions: null }))
     app.use(sendBuffer)
 
     request(app.listen())
