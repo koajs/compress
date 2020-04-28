c560be3ded487a066044eaafbe6d91aa6eb70824
jongleberry <jonathanrichardong@gmail.com>
2020-04-27 20:08:27 -0700

    brotli support (#99)
    * Added native Brotli support.
    
    * Native support if Brotli is available in Node.
    * Separate options via `brotliOptions`.
    * `brotliOptions` is `null` => skip Brotli.
    * New CI targets to test more platforms.
    * README is amended with the Brotli-specific docs.
    * New comprehensive tests for all new functionality.
    
    * Updated:
    
    * `brotliOptions` => `br`
    * `options.XXX` style for custom options, where XXX is an encoding:
      br, gzip, or deflate.
    * One test always fails because brotli is never selected.
      * Obviously I can force it for the test, but...
    * No PR is updated yet pending the decision on how to proceed.
    
    * No need for spread, the method takes an array as well.
    
    * move files to lib
    
    * fix test
    
    * add content negotiator
    
    * add tests
    
    * all tests pass
    
    * refactor encodings into a class
    
    * readme++
    
    Co-authored-by: Eugene Lazutkin <eugene.lazutkin@gmail.com>

---
+++ __tests__/encodings.js
@@ -0,0 +1,111 @@
+
+const assert = require('assert')
+
+const Encodings = require('../lib/encodings')
+
+describe('parseAcceptEncoding', () => {
+  const fixtures = [
+    {
+      input: 'br, gzip, compress, deflate',
+      output: {
+        br: 1,
+        gzip: 1,
+        compress: 1,
+        deflate: 1,
+        identity: undefined
+      }
+    },
+    {
+      input: 'br, *; q = 0.1',
+      output: {
+        br: 1,
+        gzip: 0.1,
+        compress: undefined,
+        deflate: 0.1,
+        identity: undefined
+      }
+    },
+    {
+      input: '*, gzip;q=0',
+      output: {
+        br: undefined,
+        gzip: 0,
+        compress: undefined,
+        deflate: 1,
+        identity: undefined
+      }
+    },
+    {
+      input: 'identity',
+      output: {
+        br: undefined,
+        gzip: undefined,
+        compress: undefined,
+        deflate: undefined,
+        identity: 1
+      }
+    },
+    {
+      input: 'gzip;q=0.8, identity;q=0.5, *;q=0.3',
+      output: {
+        br: undefined,
+        gzip: 0.8,
+        compress: undefined,
+        deflate: 0.3,
+        identity: 0.5
+      }
+    }
+  ]
+
+  fixtures.forEach((fixture) => {
+    test(fixture.input, () => {
+      const encodings = new Encodings()
+      encodings.parseAcceptEncoding(fixture.input)
+      const { encodingWeights } = encodings
+
+      Object.keys(fixture.output).forEach((encoding) => {
+        const expected = fixture.output[encoding]
+        const actual = encodingWeights.get(encoding)
+        assert.strictEqual(actual, expected, `Expected ${encoding} to have weight ${expected}, got ${actual}.`)
+      })
+    })
+  })
+})
+
+describe('getPreferredContentEncoding', () => {
+  const fixtures = [
+    {
+      acceptEncoding: 'gzip, br',
+      preferredEncoding: 'br'
+    },
+    {
+      acceptEncoding: 'gzip, br, *;q=0.5',
+      preferredEncoding: 'br'
+    },
+    {
+      acceptEncoding: 'br, gzip',
+      preferredEncoding: 'br'
+    },
+    {
+      acceptEncoding: 'gzip, deflate',
+      preferredEncoding: 'gzip'
+    },
+    {
+      name: 'w/o br as a preferred encoding',
+      acceptEncoding: 'gzip, deflate, br',
+      preferredEncodings: ['gzip', 'deflate'],
+      preferredEncoding: 'gzip'
+    }
+  ]
+
+  fixtures.forEach((fixture) => {
+    test(fixture.name || fixture.acceptEncoding, () => {
+      const encodings = new Encodings({
+        preferredEncodings: fixture.preferredEncodings
+      })
+      encodings.parseAcceptEncoding(fixture.acceptEncoding)
+      const preferredEncoding = encodings.getPreferredContentEncoding()
+      assert.strictEqual(preferredEncoding, fixture.preferredEncoding)
+    })
+  })
+})
